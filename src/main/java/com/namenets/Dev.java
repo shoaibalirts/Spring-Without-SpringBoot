f9f4fe53178b8a190934329178d4a0f361288a08
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-19 18:42:50 +0200

    setter injection via property attribute in xml

--- src/main/java/com/namenets/Dev.java
+++ src/main/java/com/namenets/Dev.java
@@ -1,9 +1,21 @@
 package com.namenets;
 public class Dev {
+    private int age;
+
     public Dev(){
         System.out.println("Dev constructor is created.");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void build(){
         System.out.println("Working on awesome project");
+
     }
 }
--- src/main/java/com/namenets/Main.java
+++ src/main/java/com/namenets/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Dev obj=(Dev) context.getBean("dev");
+        Dev obj=(Dev) context.getBean("dev");
+        System.out.println(obj.getAge());
 //        obj.build();
     }
 }
